521ac6d12d20a977f4aa5d6ab1a0f5e0755a01eb
mlh2865 <durk@raidtekk.us>
2024-05-28 15:50:22 -0400

    Update Offsets.hpp

--- KernalDriver Controller/KernalDriver Controller/Offsets.hpp
+++ KernalDriver Controller/KernalDriver Controller/Offsets.hpp
@@ -5,15 +5,16 @@
 
 namespace Offsets
 {
-    constexpr std::ptrdiff_t dwEntityList = 0x17ADAF0;
-    constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x16B9398;
-    constexpr std::ptrdiff_t dwViewMatrix = 0x180C0F0;
-    constexpr std::ptrdiff_t m_iTeamNum = 0x3BF;
-    constexpr std::ptrdiff_t m_iHealth = 0x32C;
-    constexpr std::ptrdiff_t m_hPlayerPawn = 0x7BC;
-    constexpr std::ptrdiff_t m_sSanitizedPlayerName = 0x720;
-    constexpr std::ptrdiff_t m_clrRender = 0xA73;
-    constexpr std::ptrdiff_t m_vecOrigin = 0x1224;
-    constexpr std::ptrdiff_t m_pGameSceneNode = 0x310;
-    constexpr std::ptrdiff_t m_ArmorValue = 0x1508;
+    constexpr std::ptrdiff_t dwEntityList = 0x19A3328;
+    constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x180DB18;
+    constexpr std::ptrdiff_t dwViewMatrix = 0x1A052D0;
+    //client.dll.cs
+    constexpr std::ptrdiff_t m_iTeamNum = 0x3C3;
+    constexpr std::ptrdiff_t m_iHealth = 0x324;
+    constexpr std::ptrdiff_t m_hPlayerPawn = 0x7DC;
+    constexpr std::ptrdiff_t m_sSanitizedPlayerName = 0x740;
+    constexpr std::ptrdiff_t m_clrRender = 0xA7B;
+    constexpr std::ptrdiff_t m_vecOrigin = 0x548;
+    constexpr std::ptrdiff_t m_pGameSceneNode = 0x308;
+    constexpr std::ptrdiff_t m_ArmorValue = 0x22C0;
 }
